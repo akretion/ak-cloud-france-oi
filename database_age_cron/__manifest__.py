57d6a35c4a461f0912e091ee3d6bfb454532dd7d
hparfr <raphael.reverdy@akretion.com>
2024-10-23 15:38:43 +0200

    [MIG] database_age_cron: Migration to 18.0

--- database_age_cron/__manifest__.py
+++ database_age_cron/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Database Age Cron",
     "summary": "Run a cron that determines database age",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/akretion/ak-cloud-france-oi",
     "author": " Akretion",
