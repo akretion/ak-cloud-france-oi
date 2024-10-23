3e414ee41194d696c919c97d6fb874cef2fcd61b
hparfr <raphael.reverdy@akretion.com>
2024-10-23 15:41:04 +0200

    [MIG] auth_oidc_akretion_data: Migration to 18.0

--- auth_oidc_akretion_data/__manifest__.py
+++ auth_oidc_akretion_data/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Auth Oidc akretion configuration data",
     "summary": """
         This module add auth oidc configuration data for akretion""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion",
     "website": "https://github.com/akretion/ak-cloud-france-oi",
